83fa61a23f051c159649172ea8f287e6b72b2381
Syncxv <aruldevarackal1@gmail.com>
2022-02-04 11:29:24 +0530

    mouse effects :O

--- src/main.js
+++ src/main.js
@@ -4,34 +4,29 @@ canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 window.ctx = ctx;
 console.log(ctx);
+const SIZE = 4;
 let h = 0;
 let particleArr = [];
 window.particleArr = particleArr;
 class Particle {
-    constructor(x, y, size) {
-        this.x = x || Math.random() * canvas.width;
-        this.y = y || Math.random() * canvas.height;
+    constructor(size, hehe) {
+        this.x = mouse.x + Math.random() * 30;
+        this.y = mouse.y + Math.random() * 30;
         this.size = size || Math.random() * 3 + 1.5;
-        console.log(window.angle);
-        this.velocity = window.angle ? Math.cos(window.angle) : Math.random() * 3.5;
+        this.opaicty = 0.5;
+        this.speedX = Math.random() * 10;
+        this.speedY = Math.random() * 1 * hehe || 1;
+        // this.velocity = window.angle ? Math.cos(window.angle) : Math.random() * 3.5;
     }
-    update() {
-        this.x += this.getVelocityX();
-        this.y += this.getVelocityY();
-        if (this.y >= canvas.height) {
-            this.y = 0;
-
-            this.x = this.getVelocityY() * -1;
-        }
-        if (this.x >= canvas.width) {
-            this.y = this.getVelocityY() * -1;
-
-            this.x = 0;
-        }
+    update(x, y) {
+        this.opaicty -= 0.009;
+        this.size -= 0.01;
+        this.x += this.speedX;
+        this.y += this.speedY;
     }
     draw() {
         ctx.beginPath();
-        ctx.fillStyle = `hsl(${h},100%,50%)`;
+        ctx.fillStyle = `hsla(${h},100%,50%, ${this.opaicty})`;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
     }
@@ -42,24 +37,31 @@ class Particle {
         return window.angle ? Math.sin(window.angle) : Math.random() * 3.5;
     }
 }
-const init = () => {
-    for (let i = 0; i < 1000; ++i) particleArr.push(new Particle());
-};
-init();
+const mouse = {};
+addEventListener("mousemove", (e) => {
+    window.angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2);
+    mouse.x = e.x;
+    mouse.y = e.y;
+    if (e.buttons !== 0) console.log(e);
+});
+addEventListener("click", () => {
+    [...Array(50)].forEach((s) => particleArr.push(new Particle(SIZE, 5)));
+});
+let n = 0;
 const animate = () => {
-    ctx.globalAlpha = 0.05;
-    ctx.fillStyle = "rgb(0,0,0)";
+    // ctx.globalAlpha = 0.05;
+    n++;
+    ctx.fillStyle = "rgb(0,0,0, 0.4)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    h += 1.1;
-    particleArr.forEach((particle) => {
+    h += 0.9;
+    particleArr.forEach((particle, i) => {
         particle.update();
         particle.draw();
+        if (particle.opaicty <= 0 || particle.size <= 0) particleArr.splice(i, 1);
     });
+    console.log(particleArr.length);
+
+    if (n % 2) particleArr.push(new Particle(SIZE));
     requestAnimationFrame(animate);
 };
 animate();
-
-addEventListener("mousemove", (e) => {
-    window.angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2);
-    console.log(angle);
-});
